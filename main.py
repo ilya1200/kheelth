ff116f59b4531b8542aeec2ed319e354fe721557
ilya1200 <ilya1200@gmail.com>
2022-02-17 20:35:03 +0200

    add filter and extract

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-# This is a sample Python script.
+import json
+import datetime
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+WEATHER_JSON: str = "weather.json"
 
 
-# Press the green button in the gutter to run the script.
+def get_daily_weather(path: str) -> list:
+    with open(path) as weather_json:
+        weather_data: dict = json.loads(weather_json.read())
+    return weather_data["daily"]
+
+def filter_rainy_days(daily_weather: list) -> list:
+    return list(filter(lambda dw: dw['weather'][0]['main'] == 'Rain', daily_weather))
+
+def extract_date_from_day(day: dict) -> str:
+    return datetime.datetime.fromtimestamp(day['dt']).strftime('%d %b %Y')
+
 if __name__ == '__main__':
-    weather_data: dict = read_from_api()
-    rainy_day: dict = find_rainy_day()
-    if not rainy_day:
-        rainy_day = days[-1]
+    weather_data: list = get_daily_weather(WEATHER_JSON)
+    rainy_days: list = filter_rainy_days(weather_data)
 
-    json_result = to_json(rainy_day)
-    write_to_webhook(json_result)
+    response = {
+        "Answer":  "Yes" if len(rainy_days) else "No",
+        "Date": extract_date_from_day(rainy_days[0] if len(rainy_days) else weather_data[-1])
+    }
 
-
+    # write_to_webhook(json_result)
