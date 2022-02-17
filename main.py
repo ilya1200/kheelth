cc79990da82416386f87548738f9b28f9648dfd1
ilya1200 <ilya1200@gmail.com>
2022-02-17 21:04:06 +0200

    add sort

--- main.py
+++ main.py
@@ -9,18 +9,32 @@ def get_daily_weather(path: str) -> list:
         weather_data: dict = json.loads(weather_json.read())
     return weather_data["daily"]
 
+
 def filter_rainy_days(daily_weather: list) -> list:
     return list(filter(lambda dw: dw['weather'][0]['main'] == 'Rain', daily_weather))
+
+
+def sort_by_date(days: list) -> list:
+    sorted_days: list = sorted(days, key=lambda s: int(s), reverse=True)
+    return sorted_days
+
+
+def humanize_unixtime(unix_time):
+    time = datetime.datetime.fromtimestamp(int(unix_time)).strftime('%d-%m-%Y %H.%M')
+    return time
+
 
 def extract_date_from_day(day: dict) -> str:
     return datetime.datetime.fromtimestamp(day['dt']).strftime('%d %b %Y')
 
+
 if __name__ == '__main__':
     weather_data: list = get_daily_weather(WEATHER_JSON)
     rainy_days: list = filter_rainy_days(weather_data)
+    rainy_days: list = sort_by_date(rainy_days)
 
-    response = {
-        "Answer":  "Yes" if len(rainy_days) else "No",
+    response: dict = {
+        "Answer": "Yes" if len(rainy_days) else "No",
         "Date": extract_date_from_day(rainy_days[0] if len(rainy_days) else weather_data[-1])
     }
 
